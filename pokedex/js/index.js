acc96523119b293eab544b7cb5279a9c8bf0543d
Thiago <104689440+thiagosctto@users.noreply.github.com>
2023-05-16 22:59:49 -0300

    Add files via upload

---
+++ pokedex/js/index.js
@@ -0,0 +1,18 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema")
+const body = document.querySelector("body")
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener('click', ()=>{
+    const modoEscuroAtivo = body.classList.contains("modo-escuro");
+
+    if (modoEscuroAtivo){
+        body.classList.remove("modo-escuro");
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png")
+    }else{
+        body.classList.add("modo-escuro")
+        imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png")
+    }
+
+   
+  
+});
